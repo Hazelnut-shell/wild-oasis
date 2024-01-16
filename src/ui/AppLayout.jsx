4f06b7c84e529568a7e4c7a3e44feeeb732a938f
yinghou <yinghouuu@gmail.com>
2024-01-15 18:19:08 -0600

    fix bugs

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-import Sidebar from "./SIdebar";
+import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
 const Main = styled.main`
